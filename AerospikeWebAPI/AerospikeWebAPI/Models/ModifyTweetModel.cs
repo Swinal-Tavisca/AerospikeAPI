ffffff4cf68ec75a620a11116d671dbba073ca6f
Swinal Kamble <swinal@tavisca.com>
2018-09-29 20:46:31 +0530

    Aerospike Web API Implemented

---
+++ AerospikeWebAPI/AerospikeWebAPI/Models/ModifyTweetModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AerospikeWebAPI.Models
+{
+    public class ModifyTweetModel
+    {
+        public string Value { get; set; }
+    }
+}
---
+++ AerospikeWebAPI/AerospikeWebAPI/Models/TweetModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AerospikeWebAPI.Models
+{
+    public class TweetModel
+    {
+        public long id { get; set; }
+        public string text { get; set; }
+        public string statusSource { get; set; }
+    }
+}
